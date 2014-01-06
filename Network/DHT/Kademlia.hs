85e2c193e5355d5623311622ab28eb90860df487
Brandon Cook <phylake@gmail.com>
2014-01-05 21:32:30 -0800

    prototype wip

---
+++ Network/DHT/Kademlia.hs
@@ -0,0 +1,143 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards #-}
+module Network.DHT.Kademlia (runKademlia) where
+
+import           Control.Concurrent
+import           Control.Concurrent.MVar (modifyMVar_, newMVar, withMVar, MVar)
+import           Control.Concurrent.Suspend.Lifted
+import           Control.Concurrent.Timer
+import           Control.Monad
+import           Data.Binary
+import           Data.Word
+import           Network.Socket
+import           System.Environment
+import           System.Posix.Signals (installHandler, Handler(Catch), sigINT, sigTERM)
+import           System.Timeout
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+import qualified Network.Socket.ByteString as NB
+
+{-
+160 bits
+k-bucket = list = [(IP, Port, NodeId)]
+1 bucket per bit that is of length k
+k is some system wide constant
+-}
+
+-- | System-wide constant
+k :: Int
+k = 20
+
+bits :: Int
+bits = 3
+
+type NodeId = Integer
+type LastSeen = Integer
+
+-- | Sorted by LastSeen
+type KBucket = V.Vector (SockAddr, NodeId, LastSeen)
+
+data RPC = RPC_UNKNOWN
+         | RPC_PING
+         | RPC_STORE
+         | RPC_FIND_NODE NodeId
+         | RPC_FIND_VALUE
+         deriving (Show)
+
+instance Binary RPC where
+  put (RPC_PING) = putWord8 1
+  put (RPC_STORE) = putWord8 2
+  put (RPC_FIND_NODE _) = putWord8 3
+  put (RPC_FIND_VALUE) = putWord8 4
+  
+  get = do
+    w <- getWord8
+    case w of
+      1 -> return $ RPC_PING
+      2 -> return $ RPC_STORE
+      3 -> return $ RPC_FIND_NODE 0
+      4 -> return $ RPC_FIND_VALUE
+      otherwise -> return RPC_UNKNOWN
+
+handler :: MVar Int -> IO ()
+handler sigMV = modifyMVar_ sigMV (return . (+1))
+
+data Hooks = Hooks {
+                     hkPing :: (Int -> IO ())
+                   , hkPing2 :: (Int -> IO ())
+                   }
+
+type DataStore = (B.ByteString -> IO (Maybe B.ByteString))
+
+runKademlia :: DataStore -> IO ()
+runKademlia ds = do
+  (delaySecs:myport:args) <- getArgs
+  sock <- socket AF_INET Datagram defaultProtocol
+  addr <- inet_addr "127.0.0.1"
+  let mySockAddr = SockAddrInet (PortNum $ read myport) addr
+  bind sock mySockAddr
+  
+  case args of
+    (yourport:[]) -> void . forkIO $ do
+      threadDelay $ microToSec $ read delaySecs
+      let yourSockAddr = SockAddrInet (PortNum $ read yourport) addr
+      connect sock yourSockAddr
+      NB.sendAll sock $ BL.toStrict $ encode RPC_PING
+    _ -> return ()
+  
+  t <- repeatedTimer
+    --(NB.sendAll sock $ BL.toStrict $ encode RPC_PING)
+    (return ())
+    (sDelay $ read delaySecs)
+  
+  {-sigMV <- newMVar 0
+  installHandler sigINT (Catch $ handler sigMV) Nothing
+  installHandler sigTERM (Catch $ handler sigMV) Nothing-}
+
+  forever $ do
+    (bs, sockAddr) <- NB.recvFrom sock 512
+    let rpc :: RPC = decode $ BL.fromStrict bs
+    case rpc of
+      RPC_PING -> do
+        connect sock sockAddr
+        NB.sendAll sock "PONG"
+        putStrLn $ "received: " ++ (show rpc)
+      _ -> do
+        putStrLn $ "received: " ++ (show $ BC.unpack bs)
+
+    --(bs, _) <- NB.recvFrom sock 512
+
+  let cleanup = do {
+    putStrLn "cleaning up";
+    close sock;
+    stopTimer t;
+  }
+    
+  --loop sock sigMV cleanup
+  return ()
+
+loop :: Socket -> MVar Int -> IO () -> IO ()
+loop sock sigMV cleanup = do
+  return ()
+  --mData <- timeout (microToSec 4) $ B.recvFrom sock 0x200
+
+microToSec :: Int -> Int
+microToSec t = t * fromIntegral (10 ** 6 :: Double)
+
+instance Integral Float where
+  quotRem a b = (fab, (ab - fab)*b)
+    where
+      ab = a/b
+      fab = floor ab
+  toInteger = floor
+
+instance Integral Double where
+  quotRem a b = (fab, (ab - fab)*b)
+    where
+      ab = a/b
+      fab = floor ab
+  toInteger = floor
