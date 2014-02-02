4e9d286257ed58c2770709552f47604a6e2e7ffe
Brandon Cook <phylake@gmail.com>
2014-02-02 13:30:10 -0800

    rpc STORE reassembly

--- Network/DHT/Kademlia.hs
+++ Network/DHT/Kademlia.hs
@@ -10,8 +10,9 @@ import           Control.Concurrent.Suspend.Lifted (sDelay)
 import           Control.Concurrent.Timer
 import           Control.Monad
 import           Data.Binary
-import           Data.Vector ((!))
+import           Data.Vector ((!), (//))
 import           Data.Word
+import           GHC.Conc.Sync (getNumCapabilities)
 import           Network.DHT.Kademlia.Bucket
 import           Network.DHT.Kademlia.Def
 import           Network.DHT.Kademlia.Util
@@ -22,14 +23,22 @@ import           System.Timeout
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.HashTable.IO as H
 import qualified Data.Vector as V
 import qualified Network.Socket.ByteString as NB
 
 handler :: MVar Int -> IO ()
 handler sigMV = modifyMVar_ sigMV (return . (+1))
 
-runKademlia :: DataStore -> IO ()
-runKademlia ds = do
+cthreads = 1
+wait mv = do
+  c <- readMVar mv
+  if c == cthreads
+    then return ()
+    else wait mv
+
+runKademlia :: KademliaEnv -> IO ()
+runKademlia env = do
   -- init networking
   -- apparently this is also needed on unix without -threaded
   -- http://hackage.haskell.org/package/network-2.4.0.1/docs/Network-Socket.html#v:withSocketsDo
@@ -38,6 +47,7 @@ runKademlia ds = do
   -- "0x" ++ `cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32`
 
   (rt :: RoutingTable) <- atomically defaultRoutingTable
+  --ctx <- zmqContext
 
   (delaySecs:myport:args) <- getArgs
   
@@ -46,51 +56,86 @@ runKademlia ds = do
   let mySockAddr = SockAddrInet (PortNum $ read myport) addr
   
   case args of
-    (yourport:[]) -> void . replicateM 20 . forkIO $ do
-      threadDelay $ secToMicro $ read delaySecs
-      let yourSockAddr = SockAddrInet (PortNum $ read yourport) addr
-      NB.sendAllTo sock (BL.toStrict $ encode RPC_PING) yourSockAddr
-    _ -> return ()
-  
-  t <- repeatedTimer
-    --(NB.sendAll sock $ BL.toStrict $ encode RPC_PING)
-    (return ())
-    (sDelay $ read delaySecs)
-  
-  {-sigMV <- newMVar 0
-  installHandler sigINT (Catch $ handler sigMV) Nothing
-  installHandler sigTERM (Catch $ handler sigMV) Nothing-}
-  
-  bind sock mySockAddr
-  forever $ do
-    (bs, sockAddr) <- NB.recvFrom sock recvBytes
-    forkIO $ do
-      let send = flip (NB.sendAllTo sock) sockAddr
-      let rpc :: RPC = decode $ BL.fromStrict bs
-      case rpc of
-        RPC_PING -> do
-          send "PONG"
-          putStrLn $ "received: " ++ (show rpc)
-        RPC_STORE _ _ _ _ _ -> do
-          return ()
-        RPC_FIND_NODE _ -> do
-          return ()
-        RPC_FIND_VALUE -> do
-          return ()
-        _ -> do
-          putStrLn $ "received: " ++ (BC.unpack bs)
-          return ()
+    (yourport:[]) -> do
+      mv <- newMVar 0
+      void . replicateM cthreads . forkIO $ do
+        threadDelay $ secToMicro $ read delaySecs
+        let yourSockAddr = SockAddrInet (PortNum $ read yourport) addr
+        forM_ [0..1] $ \i -> do
+          let bs = BL.toStrict $ encode $ RPC_STORE "c3969900f0616ef5943a" (fromIntegral i) 2 9 "abcd efgh"
+          NB.sendAllTo sock bs yourSockAddr
+          --NB.sendAllTo sock "PING" yourSockAddr
+        c <- takeMVar mv
+        putMVar mv $ c+1
+      wait mv
+    _ -> do
+      t <- repeatedTimer
+        --(NB.sendAll sock $ BL.toStrict $ encode RPC_PING)
+        (return ())
+        (sDelay $ read delaySecs)
+      
+      (storeHT :: StoreHT) <- H.new
+      mvStoreHT <- newMVar storeHT
 
-  let cleanup = do {
-    putStrLn "cleaning up";
-    close sock;
-    stopTimer t;
-  }
-    
-  --loop sock sigMV cleanup
+      bind sock mySockAddr
+      caps <- getNumCapabilities
+      --void $ replicateM (caps - 1) $ forkIO $ loop mvStoreHT sock
+      loop mvStoreHT sock
+
   return ()
 
-loop :: Socket -> MVar Int -> IO () -> IO ()
-loop sock sigMV cleanup = do
-  return ()
-  --mData <- timeout (secToMicro 4) $ B.recvFrom sock 0x200
+type StoreHT = H.BasicHashTable B.ByteString (TVar (V.Vector B.ByteString))
+type MVStoreHT = MVar StoreHT
+
+loop :: MVStoreHT -> Socket -> IO ()
+loop mvStoreHT sock = forever $ do
+  (bs, sockAddr) <- NB.recvFrom sock recvBytes
+  forkIO $ do
+    let send = flip (NB.sendAllTo sock) sockAddr
+    let rpc :: RPC = decode $ BL.fromStrict bs
+    case rpc of
+      RPC_PING -> do
+        --send "PONG"
+        putStrLn $ "received: " ++ (show rpc)
+      (RPC_STORE k n m l bs) -> do
+        storeHT <- takeMVar mvStoreHT -- TAKE MVar
+        mtvVec <- H.lookup storeHT k
+
+        tvVec <- case mtvVec of
+          Nothing -> do
+            tvVec <- atomically $ newTVar $ V.replicate (fromIntegral m) B.empty
+            H.insert storeHT k tvVec
+            putMVar mvStoreHT storeHT -- PUT MVar
+            return tvVec
+          Just tvVec -> do
+            putMVar mvStoreHT storeHT -- PUT MVar
+            return tvVec
+
+        mAssm <- atomically $ do
+          vec <- readTVar tvVec
+          let vec' = vec // [(fromIntegral n, bs)]
+          writeTVar tvVec vec'
+          return $ tryReassemble vec'
+        
+        case mAssm of
+          Nothing -> return ()
+          Just value -> do
+            putStrLn $ "have value [" ++ BC.unpack value ++ "]"
+            -- TODO does H.delete need a lock?
+            storeHT <- takeMVar mvStoreHT -- TAKE MVar
+            H.delete storeHT k
+            putMVar mvStoreHT storeHT -- PUT MVar
+        return ()
+      RPC_FIND_NODE _ -> do
+        return ()
+      RPC_FIND_VALUE -> do
+        return ()
+      _ -> do
+        putStrLn $ "received: " ++ (BC.unpack bs)
+        return ()
+
+-- TODO validate checksum
+tryReassemble :: V.Vector B.ByteString -> Maybe B.ByteString
+tryReassemble v = if V.any (== B.empty) v
+  then Nothing
+  else Just $ V.foldl1 B.append v
