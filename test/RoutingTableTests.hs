f00d344324913b0871957fca5ca3d02716909889
Brandon Cook <phylake@gmail.com>
2015-02-15 09:20:35 -0800

    formatting

--- test/RoutingTableTests.hs
+++ test/RoutingTableTests.hs
@@ -10,27 +10,27 @@ routingTableSpec = describe "routing table" $ do
       addFirstNode `shouldReturn` True
   
   describe "node 0 adding" $ do
-    it "[5,4] yields [[], [], [5,4]]" $
+    it "[5,4]           yields [[], [], [5,4]]" $
       addNodeSimple 0 [5,4] `shouldReturn` [[], [], [5,4]]
-    it "[5,4,3] yields [[], [3], [5,4]]" $
+    it "[5,4,3]         yields [[], [3], [5,4]]" $
       addNodeSimple 0 [5,4,3] `shouldReturn` [[], [3], [5,4]]
-    it "[5,4,3,2] yields [[], [3,2], [5,4]]" $
+    it "[5,4,3,2]       yields [[], [3,2], [5,4]]" $
       addNodeSimple 0 [5,4,3,2] `shouldReturn` [[], [3,2], [5,4]]
-    it "[5,4,3,2,1,6] yields [[1], [3,2], [5,4]]" $
+    it "[5,4,3,2,1,6]   yields [[1], [3,2], [5,4]]" $
       addNodeSimple 0 [5,4,3,2,1,6] `shouldReturn` [[1], [3,2], [5,4]]
     it "[5,4,3,2,1,6,7] yields [[1], [3,2], [5,4]]" $
       addNodeSimple 0 [5,4,3,2,1,6,7] `shouldReturn` [[1], [3,2], [5,4]]
-    it "[2,4] yields [[], [2], [4]]" $
+    it "[2,4]           yields [[], [2], [4]]" $
       addNodeSimple 0 [2,4] `shouldReturn` [[], [2], [4]]
-    it "[2,4,5] yields [[], [2], [4,5]]" $
+    it "[2,4,5]         yields [[], [2], [4,5]]" $
       addNodeSimple 0 [2,4,5] `shouldReturn` [[], [2], [4,5]]
-    it "[2,4,5,3] yields [[], [2,3], [4,5]]" $
+    it "[2,4,5,3]       yields [[], [2,3], [4,5]]" $
       addNodeSimple 0 [2,4,5,3] `shouldReturn` [[], [2,3], [4,5]]
-    it "[2,4,5,3,1] yields [[1], [2,3], [4,5]]" $
+    it "[2,4,5,3,1]     yields [[1], [2,3], [4,5]]" $
       addNodeSimple 0 [2,4,5,3,1] `shouldReturn` [[1], [2,3], [4,5]]
   
   describe "node 7 adding" $ do
-    it "[0,1,2,3,4,5,6]" $
+    it "[0,1,2,3,4,5,6] yields [[6],[4,5],[0,1]]" $
       addNodeSimple 7 [0,1,2,3,4,5,6] `shouldReturn` [[6],[4,5],[0,1]]
 
 newNode :: Node
